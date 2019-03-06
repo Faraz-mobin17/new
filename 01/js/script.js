071a32089769ddfbe31afce38c0c0da1667a1797
Farazmobin <faraz.mobin17@gmail.com>
2019-03-06 18:37:30 +0530

    Add files via upload

---
+++ 01/js/script.js
@@ -0,0 +1,12 @@
+function openNav() {
+		document.getElementById('myNav').style.height = '100%';
+		// document.getElementById('main').style.marginLeft = '250px';
+		// document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+
+	}
+
+	function closeNav() {
+		document.getElementById('myNav').style.height = '0%';
+		// document.getElementById('main').style.marginLeft = '0';
+		// document.body.style.backgroundColor = 'white';
+	}
